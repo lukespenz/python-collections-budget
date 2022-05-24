f7d887b9a315e496c8a7cd701cdda4aa4c483943
Lucas Spencer <lucas.spencer14@gmail.com>
2022-05-23 20:05:29 -0600

    3.2

--- budget/BudgetList.py
+++ budget/BudgetList.py
@@ -1,2 +1,6 @@
-class BudgetList:
-    pass
+class BudgetList(self, budget):
+    self.budget = budget
+    self.sum_expenses = 0
+    self.expenses = []
+    self.sum_overages = 0
+    self.overages = []
